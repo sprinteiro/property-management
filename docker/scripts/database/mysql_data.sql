63726e7928152f98ae21dc99d87318708e359eb1
Jose Rodriguez <sprinteiro@gmail.com>
2025-01-23 22:27:45 +0100

    Align data load scripts for MySQL and H2

--- docker/scripts/database/mysql_data.sql
+++ docker/scripts/database/mysql_data.sql
@@ -6,6 +6,6 @@ VALUES(1, '+34676544887', 'president@propertymanagement.org', 'Sprinter', 1, 0, 
 INSERT INTO neighbour (neighbourg_id, phonenumber, email, fullname, president, vicepresident, community_id)
 VALUES(2, '+34699999999', 'vicepresident@propertymanagement.org', 'John', 0, 1, 1);
 INSERT INTO neighbour (neighbourg_id, phonenumber, email, fullname, president, vicepresident, community_id)
-VALUES(3, null, null, null, null, null, 1);
+VALUES(3, '+34888888888', 'mark@propertymanagement.org', 'Mark', null, null, 1);
 INSERT INTO neighbour (neighbourg_id, phonenumber, email, fullname, president, vicepresident, community_id)
-VALUES(4, null, null, 'Louis', null, null, 1);
+VALUES(4, '+34888888888', 'louis@propertymanagement.org', 'Louis', null, null, 1);
--- property-management-app/src/main/resources/data.sql
+++ property-management-app/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- H2 database
 INSERT INTO community (community_id, president_id, vice_president_id, address, cif)
-VALUES (1, 99, 999, '1 Canarias Street', '0384858C');
+VALUES (1, 1, 2, '1 Canarias Street', '0384858C');
 INSERT INTO neighbour (neighbourg_id, phonenumber, email, fullname, president, vicepresident, community_id)
 VALUES(1, '+34676544887', 'president@propertymanagement.org', 'Sprinter', 1, 0, 1);
 INSERT INTO neighbour (neighbourg_id, phonenumber, email, fullname, president, vicepresident, community_id)
